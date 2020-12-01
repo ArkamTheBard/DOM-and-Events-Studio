7eac58721a527dfb098161322ed4486f3dcd0943
Yardley Gutierrez <9100062+ArkamTheBard@users.noreply.github.com>
2020-11-30 20:39:00 -0600

    background changes color, confirm input, change text trying to figure out movement

--- scripts.js
+++ scripts.js
@@ -1,2 +1,49 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+let takeoffButton = null;
+let landingButton = null;
+let abortButton = null;
+let upButton = null;
+let downButton = null;
+let leftButton = null;
+let rightButton = null;
+
+let rocketImage = null;
+
+function init(){
+    takeoffButton = document.getElementById("takeoff");
+    landingButton = document.getElementById("landing");
+    abortButton = document.getElementById("missionAbort");
+
+
+    takeoffButton.addEventListener("click", function(event){
+        let status = window.confirm("Confirm that the shuttle is ready for takeoff");
+        if(status === true){
+            document.getElementById("flightStatus").innerHTML = "Shuttle in flight";
+            document.getElementById("shuttleBackground").style.backgroundColor = "blue";
+            document.getElementById("spaceShuttleHeight").innerHTML = "10,000 miles";
+        }
+    });
+
+    landingButton.addEventListener("click", function(event){
+        window.alert("The shuttle is landing. Landing gear engaged");
+        document.getElementById("flightStatus").innerHTML = "The shuttle has landed";
+        document.getElementById("shuttleBackground").style.backgroundColor = "green";
+        document.getElementById("spaceShuttleHeight").innerHTML = "0";
+    });
+
+    abortButton.addEventListener("click", function(event){
+        let status = window.confirm("Confirm that you want to abort the mission");
+        if(status === true){
+            document.getElementById("flightStatus").innerHTML = "Mission aborted";
+            document.getElementById("shuttleBackground").style.backgroundColor = "green";
+            document.getElementById("spaceShuttleHeight").innerHTML = "0";
+        }
+    });
+
+}
+
+
+
+window.onload = init;
